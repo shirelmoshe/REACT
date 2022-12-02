318dffb0bb02a6ad66507c037532df7be3b6645f
shirelmoshe <111507868+shirelmoshe@users.noreply.github.com>
2022-12-02 11:07:50 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+export * from "./about/about.pages.jsx";
+export * from "./contactus/contact.pages.jsx";
+export * from "./games/games.pages.jsx";
+export * from "./home/home.pages.jsx";
+export * from "./movies/movies.pages.jsx";
+export * from "./notfound/notfound.pages.jsx";
